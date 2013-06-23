7b7cf8297d4e9e04560eebe5dc71fd17249388bd
Shunji Konishi <shun.konishi@nifty.com>
2013-06-23 15:00:31 +0900

    Add i18n

--- app/Global.scala
+++ app/Global.scala
@@ -1,5 +1,19 @@
+import play.api.Application;
 import play.api.mvc.WithFilters;
 import jp.co.flect.play2.filters.SessionIdFilter;
 import jp.co.flect.play2.filters.AccessControlFilter;
+import jp.co.flect.util.ResourceGen;
+import java.io.File;
 
-object Global extends WithFilters(SessionIdFilter, AccessControlFilter)
+object Global extends WithFilters(SessionIdFilter, AccessControlFilter) {
+	
+	override def onStart(app: Application) {
+		//Generate messages and messages.ja
+		val defaults = new File("conf/messages");
+		val origin = new File("conf/messages.origin");
+		if (origin.lastModified > defaults.lastModified) {
+			val gen = new ResourceGen(defaults.getParentFile(), "messages");
+			gen.process(origin);
+		}
+	}
+}
