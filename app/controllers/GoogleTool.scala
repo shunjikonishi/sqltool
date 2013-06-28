231efbc80ef06f815d824dc0308e49e66eee950b
k-shunji <k-shunji@flect.co.jp>
2013-06-28 20:13:30 +0900

    Implementing GoogleTool

--- app/controllers/GoogleTool.scala
+++ app/controllers/GoogleTool.scala
@@ -15,12 +15,41 @@ object GoogleTool extends Controller {
 	def filterAction(f: Request[AnyContent] => Result): Action[AnyContent] = Action { request =>
 		if (GoogleSpreadsheetManager.enabled) {
 			f(request);
-		} else {
+		} else {         	
 			InternalServerError("Google account is not setuped.");
 		}
 	}
 	
 	def test = filterAction { implicit request =>
-		Ok(man.test);
+		val labels = List("hoge", "FUGA", "はは　は", "ひひ ひひ");
+		val sheet = man.getOrCreateSpreadsheet("SqlToolTest");
+		val worksheet = man.getOrCreateWorksheet(sheet, "fugafuga", labels);
+		man.addRow(worksheet, labels.map { s =>
+				s.toLowerCase.replaceAll("[ 　]", "");
+			}.zip(List("1", "2", "3", "4")));
+		Ok(worksheet.getTitle.getPlainText);
+	}
+	
+	def showSheet(bookName: String, sheetName: String) = filterAction { implicit request =>
+		man.getSpreadsheet(bookName) match {
+			case Some(book) =>
+				man.getWorksheet(book, sheetName) match {
+					case Some(sheet) =>
+//						val url = book.getSpreadsheetLink.getHref + "#" + sheet.getId;
+						val url = sheet.getEtag;
+//println("link: " + sheet.getLink);
+println("editlink: " + sheet.getEditLink.getHref);
+println("editlink: " + sheet.getSelfLink.getHref);
+//println("editlink: " + sheet.getMediaEditLink.getHref);
+println("htmllink: " + sheet.getHtmlLink);
+println("summary: " + sheet.getSummary);
+println("source: " + sheet.getVersionId);
+						Ok(url);
+					case None =>
+						Ok("Worksheet not found: " + bookName + "." + sheetName);
+				}
+			case None =>
+				Ok("Spreadsheet not found: " + bookName);
+		}
 	}
 }
--- app/models/GoogleSpreadsheetManager.scala
+++ app/models/GoogleSpreadsheetManager.scala
@@ -1,13 +1,30 @@
 package models;
 
+import java.sql.ResultSet;
 import java.net.URL;
+import java.net.URI;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
+import com.google.gdata.client.spreadsheet.FeedURLFactory;
+import com.google.gdata.client.spreadsheet.SpreadsheetQuery;
+import com.google.gdata.client.spreadsheet.CellQuery;
 import com.google.gdata.data.spreadsheet.SpreadsheetFeed;
+import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
+import com.google.gdata.data.spreadsheet.WorksheetEntry;
+import com.google.gdata.data.spreadsheet.CellFeed;
+import com.google.gdata.data.spreadsheet.ListFeed;
+import com.google.gdata.data.spreadsheet.ListEntry;
+import com.google.gdata.data.TextConstruct;
+import com.google.gdata.data.docs.DocumentEntry;
+import com.google.gdata.data.Category;
+import com.google.gdata.client.docs.DocsService;
+
 import scala.collection.JavaConversions._;
 
 object GoogleSpreadsheetManager {
 	
 	val APPLICATION_NAME = "flect.co.jp-SQLTool-1.0";
+	val SPREADSHEET_CATEGORY = "http://schemas.google.com/docs/2007#spreadsheet";
+	
 	private val USERNAME = sys.env.get("GOOGLE_USERNAME");
 	private val PASSWORD = sys.env.get("GOOGLE_PASSWORD");
 	
@@ -22,9 +39,9 @@ class GoogleSpreadsheetManager(username: String, password: String) {
 	import GoogleSpreadsheetManager._;
 	
 	private val service = {
-		val ret = new SpreadsheetService(APPLICATION_NAME);
-		ret.setUserCredentials(username, password);
-		ret;
+		val ss = new SpreadsheetService(APPLICATION_NAME);
+		ss.setUserCredentials(username, password);
+		ss;
 	}
 	
 	def test = {
@@ -37,5 +54,89 @@ class GoogleSpreadsheetManager(username: String, password: String) {
 		val spreadsheets = feed.getEntries();
 		
 		spreadsheets.map(_.getTitle.getPlainText).mkString("\n");
+	}
+	
+	def getSpreadsheet(title: String): Option[SpreadsheetEntry] = {
+		val urlFactory = FeedURLFactory.getDefault;
+		val spreadsheetQuery = new SpreadsheetQuery(urlFactory.getSpreadsheetsFeedUrl());
+		spreadsheetQuery.setTitleQuery(title);
+		val spreadsheetFeed = service.query(spreadsheetQuery, classOf[SpreadsheetFeed]);
+		spreadsheetFeed.getEntries.filter(_.getTitle.getPlainText == title).headOption;
+	}
+	
+	def getOrCreateSpreadsheet(title: String): SpreadsheetEntry = {
+		getSpreadsheet(title) match {
+			case Some(x) => x;
+			case None =>
+				val DOCS_FEED_URL = new URL(
+					"https://docs.google.com/feeds/default/private/full");
+				
+				val ds = new DocsService(APPLICATION_NAME);
+				ds.setUserCredentials(username, password);
+				
+				val urlFactory = FeedURLFactory.getDefault;
+				val entry = new SpreadsheetEntry();
+				entry.setTitle(TextConstruct.plainText(title));
+				entry.getCategories.add(new Category(SPREADSHEET_CATEGORY));
+				
+				ds.insert(DOCS_FEED_URL, entry);
+		}
+	}
+	
+	def getWorksheet(sheet: SpreadsheetEntry, title: String): Option[WorksheetEntry] = {
+		sheet.getWorksheets.filter(_.getTitle.getPlainText == title).headOption;
+	}
+	
+	def getOrCreateWorksheet(sheet: SpreadsheetEntry, title: String, labels: Seq[String]): WorksheetEntry = {
+		getWorksheet(sheet, title) match {
+			case Some(x) => x;
+			case None =>
+				val worksheet = new WorksheetEntry();
+				worksheet.setTitle(TextConstruct.plainText(title));
+				worksheet.setColCount(labels.size);
+				worksheet.setRowCount(100);
+				
+				val worksheetFeedUrl = sheet.getWorksheetFeedUrl();
+				val result = service.insert(worksheetFeedUrl, worksheet);
+				val cellFeedUrl = result.getCellFeedUrl();
+				
+				val cellQuery = new CellQuery(result.getCellFeedUrl());
+				cellQuery.setRange("R1C1:R1C" + labels.size);
+				cellQuery.setReturnEmpty(true);
+				val cellFeed = service.query(cellQuery, classOf[CellFeed]);
+				
+				labels.zip(cellFeed.getEntries).foreach { case (value, cell) =>
+					cell.changeInputValueLocal(value);
+					cell.update;
+				}
+				result;
+		}
+	}
+	
+	def addRow(worksheet: WorksheetEntry, values: Seq[(String, String)]): Unit = {
+		val listFeedUrl = worksheet.getListFeedUrl();
+		val listFeed = service.getFeed(listFeedUrl, classOf[ListFeed]);
+		
+		val row = new ListEntry();
+		values.foreach { case(label, value) =>
+			row.getCustomElements().setValueLocal(label, value);
+		}
+		service.insert(listFeedUrl, row);
+	}
+	
+	def addResultSet(bookName: String, sheetName: String, rs: ResultSet): Unit = {
+		val meta = rs.getMetaData;
+		val labels = for (idx <- 1 to meta.getColumnCount) yield {
+			meta.getColumnLabel(idx);
+		}
+		val book = getOrCreateSpreadsheet(bookName);
+		val sheet = getOrCreateWorksheet(book, sheetName, labels);
+		val normalizedLabels = labels.map(_.toLowerCase.replaceAll("[ 　]", ""));
+		while (rs.next) {
+			val values = for (idx <- 1 to meta.getColumnCount) yield {
+				rs.getString(idx);
+			}
+			addRow(sheet, normalizedLabels.zip(values));
+		}
 	}
 }
